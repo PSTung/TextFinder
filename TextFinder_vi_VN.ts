4b38b3d88640ae9ff56e4dff128f54f4990eca0d
Pham, Son Tung <pham.sontung@hotmail.de>
2020-12-29 03:41:14 +0100

    TextFinder

---
+++ TextFinder_vi_VN.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="TextFinder_vi_VN">
+<context>
+    <name>TextFinder</name>
+    <message>
+        <location filename="textfinder.ui" line="14"/>
+        <source>TextFinder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="textfinder.ui" line="22"/>
+        <source>Key word</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="textfinder.ui" line="32"/>
+        <source>Find</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
